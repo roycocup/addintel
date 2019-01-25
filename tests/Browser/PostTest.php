76eef2e5a3c9c25f46bae0e618a20a4dab674f3c
Rodrigo Dias <rodrigo.dias@browsergroup.com>
2019-01-25 16:20:35 +0000

    see authors next to posts

--- tests/Browser/PostTest.php
+++ tests/Browser/PostTest.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PostTest extends DuskTestCase
 {
-    public function test_can_see_a_list_of_posts()
+    public function test_can_see_a_list_of_posts_with_author_names()
     {
         $posts = factory(Post::class, 3)->create();
 
@@ -19,6 +19,9 @@ class PostTest extends DuskTestCase
                 ->assertSee($posts[0]->title)
                 ->assertSee($posts[1]->title)
                 ->assertSee($posts[2]->title)
+                ->assertSee($posts[0]->user->name)
+                ->assertSee($posts[1]->user->name)
+                ->assertSee($posts[2]->user->name)
                 ;
         });
     }
